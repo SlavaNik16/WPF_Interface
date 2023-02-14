3f4798f7122257ed2545eed4a8a1e4ed00c75c70
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2023-02-14 12:31:18 +0300

    Add files via upload

---
+++ PR5_5_Nikolaev_Halle/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR5_5_Nikolaev_Halle
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            Image = "Volk.jpg";
+        }
+
+        private void butOk_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Volk.IsChecked)
+            {
+                Image = "Volk.jpg";
+            }else if ((bool)Lisa.IsChecked)
+            {
+                Image = "Lis.jpeg";
+            }else if ((bool)Beer.IsChecked)
+            {
+                Image = "Beer.jpeg";
+            }
+            else if ((bool)Enot.IsChecked)
+            {
+                Image = "Enot.jpeg";
+            }
+        }
+        private string image;
+        public string Image
+        {
+            get => image;
+            set
+            {
+                image = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
---
+++ PR5_6_Nikolaev_Halle/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR5_6_Nikolaev_Halle
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext= this;
+            InitializeComponent();
+            Image = "Volk.jpg";
+            Str = "Волк";
+        }
+        private RelayCommand exit;
+        public RelayCommand Exit
+        {
+            get => exit ?? (exit = new RelayCommand(x => Application.Current.Shutdown()));
+        }
+
+        private string image;
+        public string Image
+        {
+            get => image;
+            set
+            {
+                image = value;
+                OnPropertyChanged();
+            }
+        }
+        private string str;
+        public string Str
+        {
+            get => str;
+            set
+            {
+                str = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void Lisa_Click(object sender, RoutedEventArgs e)
+        {
+            Image = "Lis.jpeg";
+            Str = "Лиса";
+        }
+
+        private void Enot_Click(object sender, RoutedEventArgs e)
+        {
+            Image = "Enot.jpeg";
+            Str = "Енот";
+        }
+
+        private void Volk_Click(object sender, RoutedEventArgs e)
+        {
+            Image = "Volk.jpg";
+            Str = "Волк";
+        }
+
+        private void Beer_Click(object sender, RoutedEventArgs e)
+        {
+            Image = "Beer.jpeg";
+            Str = "Медведь";
+
+        } 
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+      
+    }
+}
